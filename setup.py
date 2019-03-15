676bea1078cc6e7ee295af4d811d7124b3933ada
Patrick Michl <patrick.michl@gmail.com>
2019-03-15 11:47:10 +0100

    fixes #13

--- setup.py
+++ setup.py
@@ -45,8 +45,8 @@ def install() -> None:
         name='flib',
         version=pkg['version'],
         description=pkg['description'],
-        long_description=pathlib.Path('.', 'README.rst').read_text(),
-        long_description_content_type='text/x-rst',
+        long_description=pathlib.Path('.', 'README.md').read_text(),
+        long_description_content_type='text/markdown',
         classifiers=[
             'Development Status :: 3 - Alpha',
             'Intended Audience :: Science/Research',
